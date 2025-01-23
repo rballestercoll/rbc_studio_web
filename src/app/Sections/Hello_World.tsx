ba05d7e69cd914e4ba9e41a3940d51251c7a6df6
Rafael Ballester Coll <rballestercoll@gmail.com>
2025-01-23 17:34:38 +0100

    Corrección problema con ESLint

--- src/app/Sections/Hello_World.tsx
+++ src/app/Sections/Hello_World.tsx
@@ -5,7 +5,7 @@ const HelloWorld = () => {
       className="h-screen flex flex-col items-start justify-center px-8 md:px-16 lg:px-32"
     >
       <h2 className="text-7xl font-bold mb-6">
-        //Hello World
+        Hello World
       </h2>
       <p className="text-lg max-w-2xl text-center mb-10">
         At RBC Studio, we believe in merging design and programming.
